0fab80d07aa4237b058dc3d3abacfc28496fc07e
Joel Aduma <caesaragen@gmail.com>
2023-02-15 13:42:58 +0300

    configure routes

--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -8,29 +8,29 @@ export class UsersRoutes extends CommonRoutesConfig implements configureRoutes {
     }
 
     configureRoutes() {
-        this.app.get{`/users`, (req: express.Request, res: express.Response)=> {
+        this.app.get(`/users`, (req: express.Request, res: express.Response)=> {
             res.status(200).send(`List of users`);
-        }}
+        })
 
-        this.app.post{`/users`, (req: express.Request, res: express.Response)=> {
+        this.app.post(`/users`, (req: express.Request, res: express.Response)=> {
             res.status(200).send(`Post to users`);
-        }}
+        })
 
-        this.app.put{`/users/:userId`, (req: express.Request, res: express.Response)=> {
+        this.app.put(`/users/:userId`, (req: express.Request, res: express.Response)=> {
             res.status(200).send(`Put to ${req.params.userId}`);
-        }}
+        })
 
-        this.app.patch{`/users/:userId`, (req: express.Request, res: express.Response)=> {
+        this.app.patch(`/users/:userId`, (req: express.Request, res: express.Response)=> {
             res.status(200).send(`Patch to ${req.params.userId}`);
-        }}
+        })
 
-        this.app.delete{`/users/:userId`, (req: express.Request, res: express.Response)=> {
+        this.app.delete(`/users/:userId`, (req: express.Request, res: express.Response)=> {
             res.status(200).send(`Delete to ${req.params.userId}`);
-        }}
+        })
 
-        this.app.get{`/users/:userId`, (req: express.Request, res: express.Response)=> {
+        this.app.get(`/users/:userId`, (req: express.Request, res: express.Response)=> {
             res.status(200).send(`Get to ${req.params.userId}`);
-        }}
+        })
         
     }
 }
